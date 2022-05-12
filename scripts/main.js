f890416228add54e6f95508e074881489e358972
sasank <sasank@adobe.com>
2022-05-12 11:21:21 -0700

    use requirejs

--- scripts/main.js
+++ scripts/main.js
@@ -27,19 +27,35 @@ function HelloPortal () {
 
     // Uncomment to see window.CCEverywhere get initialized
     // window.define.amd = false;
-    ((document, url) => {
-      const script = document.createElement("script");
-      script.src = url;
-      script.onload = () => {
-        if (!window.CCEverywhere) {
-            console.log('No ccEverywhere');
-            return;
-          }
-          const ccEverywhere = window.CCEverywhere.default.initialize();
-          console.log(ccEverywhere);
-      };
-      document.body.appendChild(script);
-    })(document, CDN_URL);
+    // ((document, url) => {
+    //   const script = document.createElement("script");
+    //   script.src = url;
+    //   script.onload = () => {
+    //     if (!window.CCEverywhere) {
+    //         console.log('No ccEverywhere');
+    //         return;
+    //       }
+    //       const ccEverywhere = window.CCEverywhere.default.initialize();
+    //       console.log(ccEverywhere);
+    //   };
+    //   document.body.appendChild(script);
+    // })(document, CDN_URL);
+
+    // use CCE with requirejs
+    requirejs.config({
+      paths: {
+          CCEverywhere: CDN_URL,
+      }
+    });
+
+    require(['CCEverywhere'], function(CCEverywhere) {
+      if (!CCEverywhere) {
+        console.log('No ccEverywhere');
+        return;
+      }
+      console.log(CCEverywhere);
+      const ccEverywhere = CCEverywhere.default.initialize();
+    });
     
     return /*#__PURE__*/React.createElement(ReactPortal, {
       wrapperId: portalId
